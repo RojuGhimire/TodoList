01bd104af8db467efae11a7d6fef57712b37948c
ghimireroju@gmail.com <ghimireroju@gmail.com>
2024-02-27 22:00:26 +0545

    edit and delete added

--- Documents/My Project/todoList/src/component/Form.jsx
+++ Documents/My Project/todoList/src/component/Form.jsx
@@ -1,15 +1,16 @@
-import React, { useDebugValue, useState } from 'react';
+import React, { useState } from 'react';
 
-const Form = () => {
+const Form = ({createTodo}) => {
     const [value, setValue] = useState('')
   const handleSubmit = e => {
     e.preventDefault()
-    console.log(useDebugValue);
+    createTodo(value);
+    setValue('')
   }
 
   return (
     <form className='mb-4 font-primary w-full' onSubmit={handleSubmit}>
-        <input type='text' className='outline-none bg-transparent border border-gray-500 p-4 w-[300px] text-white mb-8 rounded placeholder:text-gray-300' placeholder='What task do you have today?' onChange={(e)=> console.log(e.target.value)}/>
+        <input type='text' className='outline-none bg-transparent border border-gray-500 p-4 w-[300px] text-white mb-8 rounded placeholder:text-gray-300' placeholder='What task do you have today?' onChange={(e)=> setValue(e.target.value)} value={value} />
         <button className='bg-gray-900 border-none p-2 text-white cursor-pointer rounded ml-2'>
             Add Task
         </button>
--- Documents/My Project/todoList/src/component/Todo.jsx
+++ Documents/My Project/todoList/src/component/Todo.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {AiFillEdit} from 'react-icons/ai'
 
-const Todo = () => {
+const Todo = ({task}) => {
   return (
-    <div>
-      
+    <div className='flex justify-between items-center bg-violet-800 text-white py-3 px-4 rounded-md mb-1 cursor-pointer'>
+      <p className='font-primary'>{task.task}</p>
+      <div>
+        <AiFillEdit className='text-xl' />
+       
+      </div>
     </div>
   );
 }
